80a5814b35fab662c39c5ecc728982a893269fd3
asw243 <asw243@cornell.edu>
2016-03-17 12:55:18 -0400

    Still Speeding up fitness function

--- collection_zone_spherical.m
+++ collection_zone_spherical.m
@@ -4,7 +4,7 @@ d = 150;
 S = size(system);
 i=1;
 
-system(:,:,1:d,5)=1;
+system(:,:,1:d,5)=1; %This limit will always be collected
 
 while i <= S(1)
     disp('CL')
@@ -17,18 +17,20 @@ while i <= S(1)
                 
                 %check for neighbors
                 for m = -d:d
-                    for n = -d:d
-                        for l = -d:d
-                            if m~=0 && n~=0 && l~=0 %if it is not the voxel analysed
-                                if ((i+m>0) && (i+m<=S(1)) && (j+n>0) && (j+n<=S(2)) && (k+l>0) && (k+l<=S(3)))%in the bounderies of the volume
-                                    if system(i+m,j+n,k+l,1)==2 %if its a QD
-                                        if system(i+m,j+n,k+l,5)==0; %not already marked
-                                            
-                                            if (m*m + n*n + l*l) <= d*d
-                                                system(i+m,j+n,k+l,5)=1;
+                    if i+m>d
+                        for n = -d:d
+                            for l = -d:d
+                                if m~=0 && n~=0 && l~=0 %if it is not the voxel analysed
+                                    if ((i+m>0) && (i+m<=S(1)) && (j+n>0) && (j+n<=S(2)) && (k+l>0) && (k+l<=S(3)))%in the bounderies of the volume
+                                        if system(i+m,j+n,k+l,1)==2 %if its a QD
+                                            if system(i+m,j+n,k+l,5)==0; %not already marked
+                                                
+                                                if (m*m + n*n + l*l) <= d*d
+                                                    system(i+m,j+n,k+l,5)=1;
+                                                    
+                                                end
                                                 
                                             end
-                                            
                                         end
                                     end
                                 end
