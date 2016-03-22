4f716cd915177fdbfa9d93f2736444f66d4702a5
Yinnon Sanders <yys4@cornell.edu>
2016-03-22 11:38:14 -0400

    added mutation only generations

--- generate_mutations.m
+++ generate_mutations.m
@@ -1,20 +1,20 @@
-function [newMatrixList, newFitnessFunction] = generate_crossovers(A, fitnessFunction, tuner, matrixList)
-% Generates a mutated generation with half the number of individual rounded
+function [newMatrixList, newFitnessFunction] = generate_mutations(A, fitnessFunction, tuner, matrixList)
+% Generates a mutated generation with half the number of individuals rounded
 % down.
 % tuner is an integer between 1 and 10 determining proximity of mutations
 % A = [x, y, z, extraParam, numberOfMatrices]
 % previousGeneration is a list of 4D matrices with A(5) members
 % fitnessFunction is a vector with A(5) members
 
-newMatrixList = cell(1, floor(A(5) / 2 ))
-newFitnessFunction = zeros( floor(A(5) / 2 ));
+newMatrixList = cell(1, A(5));
+newFitnessFunction = zeros(1,A(5));
 
 % choose matrices to mutate
 matrixIndices = randperm(A(5), floor(A(5) / 2 ));
 
 newMat = 1;
 
-for mat in matrixIndices
+for mat = 1:A(5)
     mutatedMatrix = matrixMutate(matrixList{1, mat}, A(1), tuner);
     mutatedFitnessFunction = fitnessFn(mutatedMatrix);
     if newFitnessFunction > fitnessFunction(mat)
@@ -26,8 +26,10 @@ for mat in matrixIndices
         newMatrixList{1, newMat} = matrixList{1, mat};
         newFitnessFunction(newMat) = fitnessFunction(mat);
     end
+    newMat = newMat + 1;
 end
 
-return newFitnessFunction, newMatrixList
+newMatrixList
+newFitnessFunction
     
 end
---
+++ mutations_only.m
@@ -0,0 +1,63 @@
+generationSize = 5;
+
+generationCount = 10;
+
+matrixList = cell(1,generationSize);
+for i = 1:generationSize
+    matrix = zeros(100,100,300,11);
+    matrix(:,:,1,1)=1;
+    matrix(:,:,299,1)=3;
+    matrix(:,:,2:298,1)=2;
+    matrixList{1,i} = matrix;
+end
+
+%  Assume matrix sizes are all the same.
+x = size(matrixList{1, 1}, 1);
+y = size(matrixList{1, 1}, 2);
+z = size(matrixList{1, 1}, 3);
+extraParamCount = size(matrixList{1, 1}, 4);
+A = [x, y, z, extraParamCount, generationSize];
+
+generationList = cell(1, generationCount);
+
+% Matrix to hold fitness values for each matrix
+fitnessMatrix = [generationSize, generationCount];
+
+gen = 1;
+gen
+
+for mat = 1:generationSize
+    currentMatrix = matrixList{1,mat};
+    % Calculate fitness
+    fitnessMatrix(mat, 1) = fitnessFn(currentMatrix);
+end
+
+generationList{1,1} = matrixList;
+
+for gen = 2:generationCount
+    gen
+    [generationList{1,gen},fitnessMatrix(:,gen)] = generate_mutations(A,fitnessMatrix(:,gen - 1),5,generationList{1,gen - 1});
+end
+
+figure('position', [0, 500, 3000, 1000])  % left, bottom, width, height
+filename = 'output.gif';
+
+% Put fitness values in vector so they can be plotted
+i = 1;
+fitnessVector = zeros(1,generationSize * generationCount);
+fitnessGeneration = zeros(1,generationSize * generationCount);
+for gen = 1:generationCount
+    for mat = 1:generationSize
+        fitnessVector(1,i) = fitnessMatrix(mat,gen);
+        fitnessGeneration(1,i) = gen;
+        i = i + 1;
+    end
+end
+
+f = figure();
+scatter(fitnessGeneration, fitnessVector)
+set(gca,'fontsize', 14)
+xlabel ('Generation')
+ylabel ('Fitness Function Value')
+title('Fitness Function Values Over Time')
+saveas(f, 'mutations_only.png');
