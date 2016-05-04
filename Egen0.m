cb437f84cd1e8a79d9e702345795eb0ddbebec8c
asw243 <asw243@cornell.edu>
2016-05-04 17:18:12 -0400

    Update

---
+++ Egen0.m
@@ -0,0 +1,53 @@
+function [ System ] = Egen0( System,Lightprop )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+   S=size(System);
+ 
+
+    i=1;
+    mark=0;
+   
+    
+   while i<=S(1)
+       
+       
+       j=1;
+
+         while j<=S(2)
+         
+             
+               k=S(3);
+                mark=0;
+                 d=1;
+                   while mark~=1 && k<=S(3) 
+                       
+                       if System(i,j,k,1)==1
+                         mark=mark+1;
+                         k=k+1;
+                       end
+                       
+                       if mark==0 && System(i,j,k,1)==2
+                         System(i,j,k,2)=System(i,j,k,2)+ Lightprop(4101,d)-Lightprop(4101,d+1);  
+                         k=k-1;
+                         d=d+1;
+                       end
+                       if mark==1 %&& System(i,j,k,1)==2
+                          System(i,j,k,2)=System(i,j,k,2)+ Lightprop(4101,d)-Lightprop(4101,d+1);   
+                          k=k+1;  
+                          d=d+1;
+                       end
+
+                   end
+      j=j+1;
+         end 
+       i=i+1;
+   end
+
+
+
+
+
+end
+
