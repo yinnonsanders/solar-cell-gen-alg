8e6e09f27d44a89cd63aa3a6493ea1feb9ccaee9
Yinnon Sanders <yys4@cornell.edu>
2017-02-13 12:54:12 -0500

    added parallel trial script

---
+++ parallelTrials/5/randomholesizegen.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""randomholesizegen.py: modifies rodpos.txt to contain a list of random locations of holes with random radii"""
+
+import math
+import random
+
+__author__ = "Yinnon Sanders"
+
+length = 4 # Lateral size of sample
+area = length**2 # Area of sample
+holeRadiusMu = .200 # Center of distribution of radii of holes
+holeRadiusSigma = .050 # Standard deviation of distribution of radii of holes
+totalHoleArea = 0
+fillingFraction = .3 # Filling fraction of holes
+holeList = [] # List of holes
+
+class Hole(object):
+	"""Hole with x and y coordinates and radius"""
+
+	def __init__(self, x, y, r):
+		self.x = x
+		self.y = y
+		self.r = r
+		self.area = math.pi * r**2
+
+def overlap(newHole, holeList): # check if newHole overlaps with any holes in holeList
+	for hole in holeList:
+		if (hole.x - newHole.x)**2 + (hole.y - newHole.y)**2 < (hole.r + newHole.r)**2:
+			return True
+	return False
+
+while(totalHoleArea/area < fillingFraction):
+	# create a new hole with random coordinates
+	x = random.random() * length
+	y = random.random() * length
+	r = random.gauss(holeRadiusMu, holeRadiusSigma)
+	newHole = Hole(x,y,r)
+	if not overlap(newHole, holeList):
+		# new hole does not overlap with any existing holes
+		holeList.append(newHole)
+		totalHoleArea += newHole.area
+
+# filling fraction reached
+
+rodpos = open("rodpos.txt", "w")
+
+for hole in holeList:
+	rodpos.write("%.3f" % hole.x)
+	rodpos.write("\t")
+	rodpos.write("%.3f" % hole.y)
+	rodpos.write("\t")
+	rodpos.write("%.3f" % hole.r)
+	rodpos.write("\n")
+
+rodpos.close()
--- runParallel.sh
+++ runParallel.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 SCDIR=~/solar-cell-gen-alg # change depending on file structure
-FOLDER=$SCDIR/C++/files/$1
-OUT=$FOLDER/meep.out
-TFLUXES=$FOLDER/tfluxes.txt
-RFLUXES=$FOLDER/rfluxes.txt
-ABSORPTIONS=$FOLDER/absorptions.txt
-AVGABSORPTION=$FOLDER/avgAbsorption.txt
-IFS=$', '
-NOSTRUCT=true
-NOHOLES=true
 
-mpirun -np 12 meep-openmpi no-struct=false no-holes=false rodpos=\"$FOLDER/rodpos.txt\" $SCDIR/absorption_random_2D_photonic.ctl > $OUT
+for (( i = 0; i < 7; i++ )); do
 
+	FOLDER=$SCDIR/parallelTrials/$i
+	OUT=$FOLDER/meep.out
+	TFLUXES=$FOLDER/tfluxes.txt
+	RFLUXES=$FOLDER/rfluxes.txt
+	ABSORPTIONS=$FOLDER/absorptions.txt
+	AVGABSORPTION=$FOLDER/avgAbsorption.txt
+	IFS=$', '
 
-> $TFLUXES
-> $RFLUXES
-> $ABSORPTIONS
-> $AVGABSORPTION
+	mpirun -np 4 meep-openmpi no-struct=false no-holes=false time=250 rodpos=\"$FOLDER/rodpos.txt\" $SCDIR/absorption_random_2D_photonic.ctl > $OUT
 
-grep flux1 $OUT | while read -r line; do
-	linearray=($line)
-	echo ${linearray[2]} >> $RFLUXES
-	echo ${linearray[3]} >> $TFLUXES
+	> $TFLUXES
+	> $RFLUXES
+	> $ABSORPTIONS
+	> $AVGABSORPTION
+
+	grep flux1 $OUT | while read -r line; do
+		linearray=($line)
+		echo ${linearray[2]} >> $RFLUXES
+		echo ${linearray[3]} >> $TFLUXES
+	done
+
+	python $SCDIR/findAbsorption.py $FOLDER
+
 done
-
-python $SCDIR/findAbsorption.py $FOLDER
